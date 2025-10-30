64fe47979ad53aff5c1ab5c1037f51e8e4c8eb28
Aabha Jahagirdar <70648097+AabhaJahagirdar@users.noreply.github.com>
2025-10-30 12:34:36 +0530

    Dynamically import Lottie for client-side rendering

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,6 +1,9 @@
-"use client"
+"use client";
 
-import Lottie from "lottie-react";
+import dynamic from "next/dynamic";
+
+// Dynamically import Lottie only on the client-side
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 const AnimationLottie = ({ animationPath, width }) => {
   const defaultOptions = {
@@ -8,13 +11,11 @@ const AnimationLottie = ({ animationPath, width }) => {
     autoplay: true,
     animationData: animationPath,
     style: {
-      width: '95%',
-    }
+      width: width || "95%",
+    },
   };
 
-  return (
-    <Lottie {...defaultOptions} />
-  );
+  return <Lottie {...defaultOptions} />;
 };
 
 export default AnimationLottie;
