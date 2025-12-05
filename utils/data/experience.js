972afbe28500d40ea366dbb1b37774a0dfff91c4
Aabha Jahagirdar <70648097+AabhaJahagirdar@users.noreply.github.com>
2025-12-05 11:35:03 +0530

    data commit

---
+++ utils/data/experience.js
@@ -0,0 +1,32 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Software Intern - Java Fullstack and DSA',
+    company: "TechnoWings International IT Solution",
+    duration: "(Jun 2025 - Aug 2025)"
+  },
+  {
+    id: 2,
+    title: 'Software Intern - Data Science and AIML',
+    company: "Ybi Foundation",
+    duration: "(Jun 2024 - Jul 2024)"
+  },
+  {
+    id: 3,
+    title: 'Software Intern - AIML Technology',
+    company: "TechnoWings International IT Solution",
+    duration: "(Aug 2021 - Sept 2021)"
+  },
+  {
+    id: 4,
+    title: 'Software Intern - Python With Django Framework',
+    company: "TechnoWings International IT Solution",
+    duration: "(Sep 2020 - Oct 2020)"
+  },
+  {
+    id: 5,
+    title: 'Software Intern - Android App Development',
+    company: "TechnoWings International IT Solution",
+    duration: "(Jun 2020 - Jul 2020)"
+  }
+]
