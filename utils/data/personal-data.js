887bcde47beeb401e578809fbe8934f9b7e16a51
Aabha Jahagirdar <70648097+AabhaJahagirdar@users.noreply.github.com>
2025-12-05 21:39:35 +0530

    Change resume link to new Google Docs URL
    Updated the resume link to a new Google Docs URL.

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -13,5 +13,5 @@ export const personalData = {
   stackOverflow: '',
   leetcode: "",
   devUsername: "",
-  resume: "https://drive.google.com/file/d/10YV_23bMu6vbBcoIQgaUjrNpz8aeg3HJ/view?usp=drivesdk"
+  resume: "https://docs.google.com/document/d/1G1BkRefu9LuFy5DfyO2M2SmiQRVkCd8X/edit?usp=sharing&ouid=109432099057843499411&rtpof=true&sd=true"
 }
